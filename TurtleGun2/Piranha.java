9f90adef06a48e756b4b36b2fb90316dc68b4844
Zentac42 <AndrVeld@woodland.on.ca>
2014-12-10 14:14:57 -0500

    Added Piranha

--- TurtleGun2/Piranha.java
+++ TurtleGun2/Piranha.java
@@ -1,33 +1,57 @@
 
-/**
- * Write a description of class Piranha here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Piranha
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
 
-    /**
-     * Constructor for objects of class Piranha
-     */
-    public Piranha()
-    {
-        // initialise instance variables
-        x = 0;
+public class Piranha {
+
+    private int x, y;
+    private Image image;
+    boolean visible;
+    private int width, height;
+
+    private final int BOARD_WIDTH = 1280;
+    private final int MISSILE_SPEED = 2;
+
+    public Piranha(int x, int y) {
+
+        ImageIcon ii = new ImageIcon(this.getClass().getResource("Piranha.png"));
+        image = ii.getImage();
+        visible = true;
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+        this.x = x;
+        this.y = y;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public void move() {
+        x += MISSILE_SPEED;
+        if (x > BOARD_WIDTH)
+            visible = false;
     }
 }
