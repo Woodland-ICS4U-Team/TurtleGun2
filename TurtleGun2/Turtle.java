10640ee716b1d90ce619692b3e4ace6e0d4bb59b
Jacob Brunsting <jacob.brunsting@gmail.com>
2014-12-15 13:56:21 -0500

    Revert "updated"
    This reverts commit c8700437d835685cc614444bb6b31fcb166bd042.

--- TurtleGun2/Turtle.java
+++ TurtleGun2/Turtle.java
@@ -14,7 +14,6 @@ public class Turtle {
     private int x = STARTING_X;
     private int y = TurtleGun2.getLevelHeight() / 2;
     private Image image;
-    public int turtleHit = 0;
     
     //image costructor for turte    
     public Turtle() {
@@ -49,7 +48,6 @@ public class Turtle {
     public int getDistance() {
         return distance;
     }
-    
     //Key listener to guide the turtle
     public void keyPressed(int keyCode) {
         if (keyCode == KeyEvent.VK_SPACE) {
