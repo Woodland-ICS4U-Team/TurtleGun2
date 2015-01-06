27df59e72c25d45c33fb903f5cfbb3865be83382
Zentac42 <AndrVeld@woodland.on.ca>
2015-01-06 10:18:57 -0500

    added some comments

--- TurtleGun2/Piranha.java
+++ TurtleGun2/Piranha.java
@@ -32,26 +32,27 @@ public class Piranha {
     public Image getImage() {
         return image;
     }
-
+    //accessor for the X value of the piranha
     public int getX(int i) {
         return piranhaX[i];
     }
-
+    //accessor for the Y value of the piranha
     public int getY(int i) {
         return piranhaY[i];
     }
+    //accessor to get the maximum shots allowed in a game
     public int getNumPiranhas() {
         return MAX_SHOTS;
     }
-    public void setVisible(Boolean visible) {
-        this.visible = visible;
-    }
+    //used to tell if the piranha is shot and the piranha is visible
     public boolean getVisible(int piranhaNumber) {
         return piranhaVisible[piranhaNumber];
     }
+    //used to resset the number of shots
     public void reset(){
         i = 0;
     }
+    // when this is called piranha[i] will be set true and will appear in the game, starting where the turtle is
     public void addPiranha(int x, int y) {
         if (!piranhaVisible[i]) {
             piranhaVisible[i] = true;
@@ -61,17 +62,20 @@ public class Piranha {
             i++;
         }
     }
+    // moved the piraha across the screen for every shot piranha, and checks collision detection
     public void move(int speed, Obstacle obstacle) {
         
         for (int i = 0; i < MAX_SHOTS; i++) {
             if (piranhaVisible[i]) {
                 hit = obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null));
-                if (hit != -1) {
+                if (hit != -1) { //hit = -1 if obstacle is hit, so it hides the obstacle and the piranha
                     obstacle.hideObstacle(obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null)));
                     piranhaVisible[i] = false;
                 }
-                piranhaX[i] += speed;
-                if (piranhaX[i] > BOARD_WIDTH)
+                // moves piranha at desired speed
+                piranhaX[i] += speed; 
+                // if piranha has reached the end of the board without hitting anything, makes it invisible
+                if (piranhaX[i] > BOARD_WIDTH) 
                     piranhaVisible[i] = false;
             }
         }
