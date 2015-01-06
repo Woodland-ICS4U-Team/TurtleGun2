fc8ef1dc1305b5b84f9bd355a17f2a0bc3f32177
Zentac42 <AndrVeld@woodland.on.ca>
2015-01-06 11:05:55 -0500

    commentings

--- TurtleGun2/Piranha.java
+++ TurtleGun2/Piranha.java
@@ -9,7 +9,7 @@ public class Piranha {
     private final int BOARD_WIDTH = 1280;
     private final int MISSILE_SPEED = 2;
     
-    //related to the physical image of the piranha
+    //Prianha constructs
     private String prianhaImage = "Piranha.png";
     private int x, y;
     private Image image;
@@ -20,7 +20,7 @@ public class Piranha {
     private boolean piranhaVisible[] = new boolean[15];
     private Image[] piranhaImages = new Image[15];
 
-    //Shot related
+    //Keeps track of prianhas fired
     private int i = 0;
     private int hit = 0;
     private int shotsLeft = MAX_SHOTS;
@@ -32,15 +32,15 @@ public class Piranha {
     public Image getImage() {
         return image;
     }
-    //accessor for the X value of the piranha
+    // the X value of the piranha
     public int getX(int i) {
         return piranhaX[i];
     }
-    //accessor for the Y value of the piranha
+    // the Y value of the piranha
     public int getY(int i) {
         return piranhaY[i];
     }
-    //accessor to get the maximum shots allowed in a game
+    //How many shots you can have
     public int getNumPiranhas() {
         return MAX_SHOTS;
     }
@@ -48,7 +48,7 @@ public class Piranha {
     public boolean getVisible(int piranhaNumber) {
         return piranhaVisible[piranhaNumber];
     }
-    //used to resset the number of shots
+    //used to reset the number of shots
     public void reset(){
         i = 0;
     }
@@ -62,7 +62,7 @@ public class Piranha {
             i++;
         }
     }
-    // moved the piraha across the screen for every shot piranha, and checks collision detection
+    // collision detection for piranha 
     public void move(int speed, Obstacle obstacle) {
         
         for (int i = 0; i < MAX_SHOTS; i++) {
