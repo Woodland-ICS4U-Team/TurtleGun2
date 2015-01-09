38dbf077a73060db4c9382f0f3922f3ba353d045
Thomas Graham-Binkowski <lordpotatopancake@gmail.com>
2015-01-09 10:37:36 -0500

    Added Hit Detection

--- TurtleGun2/morePiranhas.java
+++ TurtleGun2/morePiranhas.java
@@ -9,15 +9,54 @@ public class morePiranhas {
     private int STARTING_X = TurtleGun2.getLevelWidth();
     private int MAX_STARTING_Y = TurtleGun2.getLevelHeight() - IMAGE_HEIGHT - 80;
     private int MIN_STARTING_Y = 0;
+    private int piranhaX;
+    private int piranhaY;
     
-    private boolean obstacleVisible[] = new boolean [1];
-    private boolean obstacleHit[] = new boolean [1];
-    private Image piranhaImage[] = new Image[1];
+    private boolean piranhaVisible;
+    private Image piranhaImage;
     
     private Random number = new Random();
     
     public morePiranhas() {
         ImageIcon one = new ImageIcon(this.getClass().getResource("Piranha1.png"));
-        piranhaImage[0] = one.getImage();
+        piranhaImage = one.getImage();
     }
+    
+        public void piranhaSpawn() {
+        if (!piranhaVisible) {
+            piranhaVisible = true;
+            piranhaY= number.nextInt(MAX_STARTING_Y - MIN_STARTING_Y)+MIN_STARTING_Y;
+            piranhaX = STARTING_X;
+        }
+    }
+    
+    public void move(int speed, Turtle turtle) {
+        
+        if (piranhaVisible) {
+            //add hit detectyion for turtle
+            int turtleX = turtle.getX();
+            int turtleY = turtle.getY();
+            int turtleHeight = turtle.getHeight();
+            int turtleWidth = turtle.getWidth();
+
+            boolean hit = false;
+
+            if (piranhaX < turtleX + turtleWidth && piranhaX + IMAGE_WIDTH > turtleX) { //If they overlap on X
+                    if (piranhaY < turtleY + turtleHeight && piranhaY + IMAGE_WIDTH > turtleY) { // If they overlap on Y
+                        hit = true; //We hit something
+                    }
+
+            if (hit) { //hit = -1 if turtle is hit, so it hides itself
+                 //add piranha to inventory
+                 piranhaVisible = false;
+            }
+             // moves piranha at desired speed
+             piranhaX -= 5; 
+             // if piranha has reached the end of the board without hitting anything, makes it invisible
+            if (piranhaX < -20) 
+                piranhaVisible = false;
+        }
+        
+    } 
+  }
 }
