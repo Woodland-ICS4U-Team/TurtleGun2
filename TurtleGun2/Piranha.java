09651af2542ccfe16762e13663d15165b3e938f7
Zentac42 <AndrVeld@woodland.on.ca>
2015-01-15 09:24:24 -0500

    few more things in piranha

--- TurtleGun2/Piranha.java
+++ TurtleGun2/Piranha.java
@@ -1,3 +1,9 @@
+/**
+ * Class for the piranha
+ * 
+ * Andrew Veldhuis 
+ * 1/15/2015
+ */
 
 import java.awt.Image;
 import java.awt.Rectangle;
@@ -23,6 +29,41 @@ public class Piranha {
         ImageIcon ii = new ImageIcon(this.getClass().getResource(prianhaImage));
         image = ii.getImage();
     }
+   
+    // when this is called piranha[i] will be set true and will appear in the game, starting where the turtle is
+    public void addPiranha(int x, int y) {
+        // for loop will look for and invisible piranhas for it to use when a shot is made
+        for (int z = 0; z < MAX_SHOTS; z++) {
+            if (!piranhaVisible[z]) {
+                currentPiranha = z;
+            }
+        }
+        piranhaVisible[currentPiranha] = true; //level will look for all the visible piranhas and draw them
+        piranhaImages[currentPiranha] = image;
+        piranhaY[currentPiranha] = y; //sets piranha to the Turtles y value
+        piranhaX[currentPiranha] = x; //sets prianha to the Turtles x value
+        currentPiranha++;
+    }
+    
+    // collision detection and movement for piranha for piranha 
+    public void move(int speed, Obstacle obstacle) {
+        
+        for (int i = 0; i < MAX_SHOTS; i++) {
+            if (piranhaVisible[i]) {
+                hit = obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null));
+                if (hit != -1) { //hit = -1 if obstacle is hit, so it hides the obstacle and the piranha
+                    obstacle.removeObstacle(obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null)));
+                    piranhaVisible[i] = false; 
+                }
+                // moves piranha at desired speed
+                piranhaX[i] += speed; 
+                // if piranha has reached the end of the board without hitting anything, makes it invisible
+                if (piranhaX[i] > BOARD_WIDTH) 
+                    piranhaVisible[i] = false;
+            }
+        }
+        
+    } 
     
     public Image getImage() {
         return image;
@@ -52,44 +93,10 @@ public class Piranha {
             
         }
     }
-   
-    // when this is called piranha[i] will be set true and will appear in the game, starting where the turtle is
-    public void addPiranha(int x, int y) {
-        // for loop will look for and invisible piranhas for it to use when a shot is made
-        for (int z = 0; z < MAX_SHOTS; z++) {
-            if (!piranhaVisible[z]) {
-                currentPiranha = z;
-            }
-        }
-        piranhaVisible[currentPiranha] = true; //level will look for all the visible piranhas and draw them
-        piranhaImages[currentPiranha] = image;
-        piranhaY[currentPiranha] = y; //sets piranha to the Turtles y value
-        piranhaX[currentPiranha] = x; //sets prianha to the Turtles x value
-        currentPiranha++;
-    }
     
     public void addToInventory(){
         //when this is called the current piraha slot is moved back one
         currentPiranha --;
     }
     
-    // collision detection for piranha 
-    public void move(int speed, Obstacle obstacle) {
-        
-        for (int i = 0; i < MAX_SHOTS; i++) {
-            if (piranhaVisible[i]) {
-                hit = obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null));
-                if (hit != -1) { //hit = -1 if obstacle is hit, so it hides the obstacle and the piranha
-                    obstacle.removeObstacle(obstacle.checkCollisions(getX(i), getY(i), getImage().getWidth(null), getImage().getHeight(null)));
-                    piranhaVisible[i] = false; 
-                }
-                // moves piranha at desired speed
-                piranhaX[i] += speed; 
-                // if piranha has reached the end of the board without hitting anything, makes it invisible
-                if (piranhaX[i] > BOARD_WIDTH) 
-                    piranhaVisible[i] = false;
-            }
-        }
-        
-    } 
 }
